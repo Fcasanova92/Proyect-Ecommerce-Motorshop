63346e59f208b0eb7a6cb7f89610d082eb9fe15c
Fercasanov <fernando.casanova1992@outlook.com>
2024-07-17 21:14:07 -0300

    fixed ---  hay una conexion en el test

--- tests/api/routes/private/auth/auth.test.js
+++ tests/api/routes/private/auth/auth.test.js
@@ -7,7 +7,7 @@ const api = supertest(app)
 
 test('auth returned as json ', async () => {
 
-   await api.post('/api/auth/regiser')
+   await api.post('/api/auth/register')
    .expect (200)
    .expect('Content-Type', /application\/json/ )
     
@@ -18,4 +18,5 @@ afterAll(()=>{  // hook que se ejecuta luego de los test
    getConnection().end() // cerramos base de datos
 
    server.close(); // cerramos servidor
+
 })
